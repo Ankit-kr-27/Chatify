3e5a9d292a7187d8fe4c542569825b678b4fcf44
Ankit kumar <kr.ankit2783@gmail.com>
2026-01-09 20:02:38 +0530

    login and signup page ui done

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,11 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from "path";
 import cookieParser from 'cookie-parser';
-
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
+import cors from 'cors';
+import { ENV } from "./lib/env.js";
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
